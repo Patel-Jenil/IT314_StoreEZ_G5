9bd97c72813cc8c40699149094cff2c3b72910ce
Akhil Anilkumar Kanteti <202101027@daiict.ac.in>
2023-11-18 13:51:44 +0530

    Search Available Warehouses Page Updated

--- Project/frontend/Akhil/Search-Page-from-profile/storage-type.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("storageForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  const selectedStorageType = document.getElementById("storageType").value;
-  // // Perform your search using the selectedStorageType
-  // // Display the results in the "searchResults" div
-  // document.getElementById("searchResults").textContent = `Searching for ${selectedStorageType} storage...`;
-});
