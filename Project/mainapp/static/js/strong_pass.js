83a669a32f564585402ce1a5c35cde3e56fe60e9
Bhavya2104 <97469185+Bhavya2104@users.noreply.github.com>
2023-11-22 22:55:50 +0530

    Strong password must required for registration Func. made

---
+++ Project/mainapp/static/js/strong_pass.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const passwordInput = document.querySelector(".password-input--box input");
+// const eyeIcon = document.querySelector(".password-input--box i");
+const requirementsListEl = document.querySelectorAll(".requirement-list li");
+
+// console.log(parent)
+console.log(requirementsListEl)
+console.log(passwordInput)
+const requirements = [
+	{ regex: /.{8,}/, index: 0 },
+	{ regex: /[0-9]/, index: 1 },
+	{ regex: /[a-z]/, index: 2 },
+	{ regex: /[^A-Za-z0-9]/, index: 3 },
+	{ regex: /[A-Z]/, index: 4 },
+];
+
+passwordInput.addEventListener("keyup", (e) => {
+	requirements.forEach((item) => {
+		const isValid = item.regex.test(e.target.value);
+		const requirementItem = requirementsListEl[item.index];
+
+		if (isValid) {
+			console.log(requirementItem);
+			requirementItem.firstElementChild.className = "fa-solid fa-check";
+			requirementItem.classList.add("valid");
+		} else {
+			requirementItem.firstElementChild.className = "fa-solid fa-circle";
+			requirementItem.classList.remove("valid");
+		}
+	});
+});
+
+// eyeIcon.addEventListener("click", function () {
+// 	passwordInput.type =
+// 		passwordInput.type === "password" ? "text" : "password";
+
+// 	eyeIcon.className = `fa-solid fa-eye${
+// 		passwordInput.type === "password" ? "" : "-slash"
+// 	}`;
+// });
